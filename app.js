62a9f739faf7e6d6160d3de9fe0ce25e798c1a45
Thomas Jenkins <Jenkinsth@yahoo.com>
2022-08-07 22:15:10 -0700

    add render and derender functions to allow the fighter to move around the board.

--- app.js
+++ app.js
@@ -1,37 +1,139 @@
-import { renderFighterCard } from "./render-fighters.js"; 
-import { fighterTemplate } from "./fighter-data.js";
-import { renderCells } from "./render-board.js";
-import { renderFighter } from "./render-fighters.js";
 const board = document.querySelector('#board');
 const ui = document.querySelector('#ui');
 
-let cells = 64;
+let temp = '1-1';
+let numOne = 0;
+let numTwo = 1;
+let click = ['1-1'];
 
 
-for (let i = 0; i < cells; i++) {
+for (let i = 0; i < 64; i++) {
     renderCells();
 }
 renderFighterCard();
+renderFighter();
 
-// renderFighter();
+function renderCells() {
+    const cell = document.createElement('div');
+    cell.classList.add(`cell-class`);
+    if (numOne !== 8) {
+        numOne++;
+    } else {
+        numOne = 1
+        numTwo++;
+    }
+    cell.setAttribute(`id`, `${numTwo}` + `-` + `${numOne}`);
+    
+    cell.addEventListener('click', () => {
+        click.push(cell.id);
+        // click = [];
+        console.log(click);
+        renderFighter();
+        
+        deRenderFighter();
+        return click;
+        
+    });
+    board.append(cell);
+}
+
+
+
+function renderFighterCard() {
+    const fighterCard = document.createElement('div');
+    const fighterName = document.createElement('div');
+    const fighterStats = document.createElement('div');
+    const fighterHP = document.createElement('p');
+    const fighterDef = document.createElement('p');
+    const fighterResources = document.createElement('div');
+    const fighterActionPoints = document.createElement('p');
+    const fighterButtons = document.createElement('div');
+    const fighterMove = document.createElement('button');
+    const fighterAttack = document.createElement('button');
+    const fighterOther = document.createElement('button');
+
+    fighterCard.classList.add('unit-card');
+    
+
+    fighterName.textContent = 'Fighter'
+    fighterHP.textContent = 'HP 10/10'
+    fighterDef.textContent = 'Def: 12'
+    fighterActionPoints.textContent = 'Action Points: 2/2';
+    fighterMove.textContent = 'Move';
+    fighterAttack.textContent = 'Attack';
+    fighterOther.textContent = '...'
+
+    ui.append(fighterCard);
+    fighterCard.append(fighterName, fighterStats, fighterResources, fighterButtons);
+    fighterStats.append(fighterHP, fighterDef);
+    fighterResources.append(fighterActionPoints);
+    fighterButtons.append(fighterMove, fighterAttack, fighterOther);
+    
+}
+
+function renderFighter() {
+    const lastLoc = click[click.length - 1];
+    const fighterToken = document.createElement('div');
+    const fighterImg = document.createElement('div');
+    const location = document.getElementById(`${lastLoc}`);
+    fighterToken.classList.add('fighter')
+    fighterImg.classList.add('fighter-image')
+    location.append(fighterToken);
+    fighterToken.append(fighterImg);
+}
+function deRenderFighter() {
+    const tick = click[click.length -2];
+    
+    const identity = document.getElementById(`${tick}`);
+    console.log(identity);
+    identity.removeChild(identity.firstChild);
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // renderFighter(loc);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // let cellColumns = [1];
 // let cellRows = [1];
--- render-board.js
+++ render-board.js
@@ -1,14 +1,4 @@
 let numOne = 0;
 let numTwo = 1;
-export function renderCells() {
-    const cell = document.createElement('div');
-    cell.classList.add(`cell-class`);
-    if (numOne !== 8) {
-        numOne++;
-    } else {
-        numOne = 1
-        numTwo++;
-    }
-    cell.classList.add(`${numTwo}` + '-' + `${numOne}`);
-    board.append(cell);
-}
+let click = [];
+export 
